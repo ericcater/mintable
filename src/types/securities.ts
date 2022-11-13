1e9318eb09f364b1057dee572d0d804a85ac9e8c
ericcater <ecater8@gmail.com>
2022-11-13 07:42:57 +0300

    types

---
+++ src/types/securities.ts
@@ -0,0 +1,14 @@
+export interface Holdings {
+    account_id: string
+    cost_basis: number
+    institution_price: number
+    institution_price_as_of: Date
+    institution_price_datetime: Date
+    institution_value: number
+    // iso_currency_code: string,
+    quantity: number
+    security_id: string
+    // unofficial_currency_code: null
+
+    security_name: string
+}
