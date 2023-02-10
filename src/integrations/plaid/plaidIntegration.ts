a24878ca39ec6fb5fadbd8ae25a1daeea253d587
ericcater <ecater8@gmail.com>
2023-02-10 12:08:25 +0300

    remove dumb choice to abs quantity

--- src/integrations/plaid/plaidIntegration.ts
+++ src/integrations/plaid/plaidIntegration.ts
@@ -405,7 +405,7 @@ export class PlaidIntegration {
                         security_id: transaction.security_id,
                         date: parseISO(transaction.date),
                         name: transaction.name,
-                        quantity: Math.abs(transaction.quantity),
+                        quantity: transaction.quantity,
                         amount: transaction.amount,
                         price: transaction.price,
                         fees: transaction.fees,
